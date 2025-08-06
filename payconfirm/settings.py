c3dfd597210e58d9cba22b91b00367bd96c36e74
Tega Rorobi <rorobitega@gmail.com>
2025-08-06 20:40:29 +0100

    ops: Configure whitenoise settings to handle static in prod

--- payconfirm/settings.py
+++ payconfirm/settings.py
@@ -45,6 +45,12 @@ CORS_ALLOWED_ORIGINS = envvar(
     default="http://localhost,http://127.0.0.1,https://0.0.0.0"
 )
 
+
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
@@ -69,10 +75,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'debug_toolbar',
     'corsheaders',
+    "whitenoise",
 ]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -150,7 +158,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/' # at the top
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
