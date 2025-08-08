679aa32081c552559c89b4fdae325fa003f78cba
Tega Rorobi <rorobitega@gmail.com>
2025-08-08 04:04:52 +0100

    feat: Add app_name marker in main URLconf

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from .views import Home, PaymentView
 from django.urls import path
 
+app_name = 'main'
+
 paymentview_actions = {
     'get': 'list_all_payments',
     'post': 'register_new_payment',
 }
-
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('payments/', PaymentView.as_view(paymentview_actions), name='paymentview'),
